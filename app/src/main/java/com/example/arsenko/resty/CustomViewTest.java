9eb365cba9356c46af763242f827ad411a3636ef
Arsen Markiv <arsenemarkiv@gmail.com>
2017-11-22 20:26:57 +0200

    recommit

---
+++ app/src/main/java/com/example/arsenko/resty/CustomViewTest.java
@@ -0,0 +1,12 @@
+package com.example.arsenko.resty;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CustomViewTest extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/com/example/arsenko/resty/MainActivity.java
+++ app/src/main/java/com/example/arsenko/resty/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.arsenko.resty;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,117 +8,174 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.JSONTokener;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.SocketTimeoutException;
 import java.net.URL;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Iterator;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     Button people;
     Button starships;
     Button planets;
     ProgressBar progressBar;
     TextView responseView;
+
     public static final String API_URL = "https://swapi.co/api/";
-    public static final String PEOPLE_URL = "people/";
-    public static final String STARSHIPS_URL = "starships/";
-    public static final String PLANET_URL = "planet/";
+    //    public static final String PEOPLE_URL = "people/";
+//    public static final String STARSHIPS_URL = "starships/";
+//    public static final String PLANET_URL = "planet/";
     public static String STRAIGHT_URL;
-    public String response;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         people = (Button) findViewById(R.id.peopleBtn);
-        starships = (Button) findViewById(R.id.starshipsBtn);
         planets = (Button) findViewById(R.id.planetsBtn);
-        progressBar = (ProgressBar)findViewById(R.id.progressBar);
-        responseView = (TextView)findViewById(R.id.responseView);
+        starships = (Button) findViewById(R.id.starshipsBtn);
 
-    }
-
-    public String getConnection(){
-        try {
-            URL url = new URL(API_URL + STRAIGHT_URL);
-            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-            Toast.makeText(this, "open connection", Toast.LENGTH_SHORT).show();
-            try {
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
-                StringBuilder stringBuilder = new StringBuilder();
-                String line;
-                while ((line = bufferedReader.readLine()) != null) {
-                    stringBuilder.append(line).append("\n");
-                }
-                bufferedReader.close();
-                return stringBuilder.toString();
-
-            } finally {
-                if (httpURLConnection != null) {
-                    httpURLConnection.disconnect();
-                }
-            }
-        } catch (Exception e) {
-            Log.e("ERROR", e.getMessage(), e);
-            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
-            return  null;
-        }
-
-    }
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        responseView = (TextView) findViewById(R.id.responseView);
 
 
-    public void getResponse(String response){
-        if (response == null){
-            response = "Error!";
-        }
-        progressBar.setVisibility(View.GONE);
-        Toast.makeText(this, "response:" + response, Toast.LENGTH_SHORT).show();
-        responseView.setText(response);
-        try{
-            JSONObject object = (JSONObject) new JSONTokener(response).nextValue();
-            String requestID = object.getString("requestID");
-            JSONArray info = object.getJSONArray("info");
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void onClick(View v) {
-        progressBar.setVisibility(View.VISIBLE);
-        responseView.setText("");
-
-        switch (v.getId()) {
-            case R.id.peopleBtn:
+        people.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 STRAIGHT_URL = "people/";
-                getConnection();
-                getResponse(response);
-                break;
-            case R.id.starshipsBtn:
-                STRAIGHT_URL = "starships/";
-                break;
-            case R.id.planetsBtn:
-                STRAIGHT_URL = "planets/";
-                break;
-        }
+                new ForRestTask().execute();
+            }
+
+        });
+
+
+        starships.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                STRAIGHT_URL = "starships/?format=json";
+                new ForRestTask().execute();
+            }
+        });
+
+
+        planets.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                STRAIGHT_URL = "planets/?format=json";
+                new ForRestTask().execute();
+
+            }
+        });
+
+
     }
 
+
+    private class ForRestTask extends AsyncTask<Void, Void, String> {
+
+        protected void onPreExecute() {
+            progressBar.setVisibility(View.VISIBLE);
+            responseView.setText("");
+
+        }
+
+        protected String doInBackground(Void... urls) {
+            try {
+                URL url = new URL(API_URL + STRAIGHT_URL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                Log.i("log", "open connection");
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+
+                } finally {
+                    if (httpURLConnection != null) {
+                        httpURLConnection.disconnect();
+                    }
+                }
+            } catch (Exception e) {
+                Log.e("ERROR", e.getMessage(), e);
+                return null;
+            }
+
+        }
+
+        protected void onPostExecute(String response) {
+            if (response == null) {
+                response = "Error!";
+            }
+            progressBar.setVisibility(View.GONE);
+            Log.i("log", "response" + response);
+            //responseView.setText(response);
+
+            try {
+                JSONObject object = new JSONObject(response);
+                JSONArray array = object.getJSONArray("results");
+                Log.i("log", "results" + array);
+
+//                View.OnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        for(int i=0; i<array.length(); i++){
+//                            JSONObject jObject = array.getJSONObject(i);
+//                            String name = jObject.optString("name");
+//                            Log.i("log", "name: " + name);
+//                            responseView.getText();
+//                            responseView.setText(responseView.getText() + "\n" + name);
+//
+//                        }
+//                    }
+//                });
+                for(int i=0; i<array.length(); i++){
+                    JSONObject jObject = array.getJSONObject(i);
+                    String name = jObject.optString("name");
+                    Log.i("log", "name: " + name);
+                    responseView.getText();
+                    responseView.setText(responseView.getText() + "\n" + name);
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
+
+//    @Override
+//    public void onClick(View v) {
+//
+//        switch (v.getId()) {
+//            case R.id.peopleBtn:
+//                STRAIGHT_URL = "people/?format=json";
+//                ForRestTask peopleTask = new ForRestTask();
+//                peopleTask.execute();
+//                break;
+//            case R.id.starshipsBtn:
+//                STRAIGHT_URL = "starships/?format=json";
+//                ForRestTask starshipsTask = new ForRestTask();
+//                starshipsTask.execute();
+//                break;
+//            case R.id.planetsBtn:
+//                STRAIGHT_URL = "planets/?format=json";
+//                ForRestTask planetsTask = new ForRestTask();
+//                planetsTask.execute();
+//                break;
+//        }
+//    }
 //    public static JSONObject jsonObject(String API_URL){
 //        HttpURLConnection httpURLConnection = null;
 //
@@ -130,9 +188,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //            //handle issues
 //            int statusCode = httpURLConnection.getResponseCode();
 //            if(statusCode == HttpURLConnection.HTTP_UNAUTHORIZED){
-//                Log.e("log", "Authorization required");
+//                Log.e(TAG, "Authorization required");
 //            }else if(statusCode != HttpURLConnection.HTTP_OK){
-//                Log.e("log", "You have another error");
+//                Log.e(TAG, "You have another error");
 //            }
 //
 //            //create JSON
